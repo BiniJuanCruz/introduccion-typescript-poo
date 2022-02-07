03ff0d6a47014e36e98c054509258ada5065384b
ranel <binijuancruz@gmail.com>
2022-02-07 16:42:07 -0300

    Clase Json, Descarga npm y TypeScript

---
+++ poo-y-typescript/01-clase-json.js
@@ -0,0 +1,11 @@
+// Clase Json.
+var bicicleta = {
+    color: "rojo",
+    modelo: "bmx",
+    frenos: "de disco",
+    velocidadMaxima: "60km",
+    cambiaColor: function(nuevo_color){
+        this.color = nuevo.color;
+        console.log(this);
+    }
+};
