11384718f29523bc323e4a729718297bdab6d6de
ranel <binijuancruz@gmail.com>
2022-02-07 17:58:59 -0300

    Tipos de datos

---
+++ poo-y-typescript/03-tipos-de-datos.js
@@ -0,0 +1,14 @@
+// string 
+var cadena = "ranel desarrollo software";
+//number
+var numero = 12;
+//boolean
+var veradero_falso = true;
+//any
+var cualquiera = "hola";
+//arrays 
+var lenguajes = ["php", "python", "js"];
+var years = [12, 24, 45];
+//multiple tipo de dato
+var multiple = "Puede ser un numero tambien";
+var cadena2 = 123;
---
+++ poo-y-typescript/03-tipos-de-datos.ts
@@ -0,0 +1,22 @@
+// string 
+let cadena: string = "ranel desarrollo software";
+
+//number
+let numero: number = 12;
+
+//boolean
+let veradero_falso: boolean = true;
+
+//any
+let cualquiera: any = "hola";
+
+//arrays 
+var lenguajes: Array<string> = ["php", "python", "js"];
+let years: number[] = [12,24,45];
+
+//multiple tipo de dato
+let multiple: string | number = "Puede ser un numero tambien";
+
+//personalizado
+type alfaNumerico = string | number;
+let cadena2: alfaNumerico = 123;
