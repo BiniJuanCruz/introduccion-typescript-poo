335cfe847c458b9ecf8764cbfc31a588e174c778
ranel <binijuancruz@gmail.com>
2022-02-08 00:17:46 -0300

    Clases - Clase Camiseta

---
+++ poo-y-typescript/camiseta.js
@@ -0,0 +1,29 @@
+// Clase (Modelo del Objeto)
+var Camiseta = /** @class */ (function () {
+    //Metodos (funciones o acciones del objeto)
+    //Metodo constructor
+    function Camiseta(color, modelo, marca, talla, precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }
+    Camiseta.prototype.setColor = function (color) {
+        this.color = color;
+    };
+    Camiseta.prototype.getColor = function () {
+        return this.color;
+    };
+    return Camiseta;
+}());
+// instanciar un objeto
+var camiseta = new Camiseta("rojo", "manga larga", "nike", "L", 14);
+camiseta.setColor("rojo");
+console.log(camiseta.getColor());
+// asignarle propiedades si fueran public
+/*camiseta.color = "rojo";
+camiseta.modelo = "manga larga";
+camiseta.marca = "Nike";
+camiseta.talla = "L";
+camiseta.precio = 10;*/ 
---
+++ poo-y-typescript/camiseta.ts
@@ -0,0 +1,41 @@
+// Clase (Modelo del Objeto)
+class Camiseta{
+    // Propiedades (Caracteristicas del objeto)
+    private color: string;
+    private modelo: string;
+    private marca: string;
+    private talla: string;
+    private precio: number;
+    
+    //Metodos (funciones o acciones del objeto)
+
+    //Metodo constructor
+    constructor(color,modelo,marca,talla,precio){
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }
+
+    public setColor(color){
+        this.color = color;
+    }
+    public getColor(){
+        return this.color;
+    }
+}
+
+// instanciar un objeto
+
+var camiseta = new Camiseta("rojo","manga larga","nike","L",14);
+camiseta.setColor("rojo");
+console.log(camiseta.getColor());
+
+// asignarle propiedades si fueran public
+
+/*camiseta.color = "rojo";
+camiseta.modelo = "manga larga";
+camiseta.marca = "Nike";
+camiseta.talla = "L";
+camiseta.precio = 10;*/
